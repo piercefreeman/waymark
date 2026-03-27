327b77db68a4350b6cbfc58a3be161d32d944927
MOZGIII <mozgiii@mzg.io>
2026-03-27 17:29:34 +0400

    Set buckets for seconds metrics

--- crates/bin/start-workers/src/main.rs
+++ crates/bin/start-workers/src/main.rs
@@ -69,6 +69,10 @@ async fn main() -> Result<()> {
         .with_recommended_naming(true)
         .with_http_listener(metrics_addr)
         .set_bucket_duration(std::time::Duration::from_secs(600))?
+        .set_buckets_for_metric(
+            metrics_exporter_prometheus::Matcher::Suffix("_seconds".to_string()),
+            &[0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10., 30., 60., 300., 600.],
+        )?
         .install()?;
 
     // Load configuration and announce startup.
