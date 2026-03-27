e12b43f9f59b908fdeffb2485ba335306e639737
MOZGIII <mozgiii@mzg.io>
2026-03-27 17:29:34 +0400

    Set buckets for seconds metrics

--- crates/lib/prometheus-exporter-bringup/src/lib.rs
+++ crates/lib/prometheus-exporter-bringup/src/lib.rs
@@ -7,6 +7,10 @@ pub fn spawn_and_install_recorder(
         .with_recommended_naming(true)
         .with_http_listener(metrics_addr)
         .set_bucket_duration(std::time::Duration::from_secs(600))?
+        .set_buckets_for_metric(
+            metrics_exporter_prometheus::Matcher::Suffix("_seconds".to_string()),
+            &[0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10., 30., 60., 300., 600.],
+        )?
         .build()?;
 
     metrics::set_global_recorder(recorder)?;
