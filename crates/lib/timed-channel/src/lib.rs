5fead2065c8322ea974857a993ba359317e77f10
MOZGIII <mozgiii@mzg.io>
2026-03-30 19:52:14 +0400

    Add timed channel crate

---
+++ crates/lib/timed-channel/src/lib.rs
@@ -0,0 +1,119 @@
+pub mod tokio {
+    pub mod mpsc;
+
+    pub mod sync {
+        #[deprecated = "lose the `sync` mod"]
+        pub use super::mpsc;
+    }
+}
+
+pub mod std {
+    pub mod mpsc;
+}
+
+pub trait What {
+    fn description() -> &'static str;
+}
+
+#[macro_export]
+macro_rules! named_what {
+    ($name:ident, $desc:expr) => {
+        #[derive(Debug)]
+        pub struct $name;
+
+        impl $crate::What for $name {
+            fn description() -> &'static str {
+                $desc
+            }
+        }
+    };
+}
+
+pub fn wrap<Sender, Receiver, What>(
+    channels: (Sender, Receiver),
+) -> (Tx<Sender>, Rx<Receiver, What>) {
+    let (sender, receiver) = channels;
+    (Tx::new(sender), Rx::new(receiver))
+}
+
+pub struct Tx<C> {
+    pub channel: C,
+}
+
+pub struct Rx<C, What> {
+    pub channel: C,
+    pub phantom_data: core::marker::PhantomData<What>,
+}
+
+impl<C> core::ops::Deref for Tx<C> {
+    type Target = C;
+
+    fn deref(&self) -> &Self::Target {
+        &self.channel
+    }
+}
+
+impl<C> core::ops::DerefMut for Tx<C> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.channel
+    }
+}
+
+impl<C, What> core::ops::Deref for Rx<C, What> {
+    type Target = C;
+
+    fn deref(&self) -> &Self::Target {
+        &self.channel
+    }
+}
+
+impl<C, What> core::ops::DerefMut for Rx<C, What> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.channel
+    }
+}
+
+impl<C> Tx<C> {
+    pub const fn new(channel: C) -> Self {
+        Self { channel }
+    }
+}
+
+impl<C, What> Rx<C, What> {
+    pub const fn new(channel: C) -> Self {
+        Self {
+            channel,
+            phantom_data: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<C: Clone> Clone for Tx<C> {
+    fn clone(&self) -> Self {
+        Self {
+            channel: self.channel.clone(),
+        }
+    }
+}
+
+impl<C: Clone, What> Clone for Rx<C, What> {
+    fn clone(&self) -> Self {
+        Self {
+            channel: self.channel.clone(),
+            phantom_data: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<C, What> Rx<C, What> {
+    pub fn tag<NewWhat>(self) -> Rx<C, NewWhat> {
+        let Self {
+            channel,
+            phantom_data: _,
+        } = self;
+        Rx {
+            channel,
+            phantom_data: core::marker::PhantomData,
+        }
+    }
+}
---
+++ crates/lib/timed-channel/src/tokio/mpsc.rs
@@ -0,0 +1,62 @@
+use crate::{Rx, Tx};
+
+pub type Sender<T> = Tx<tokio::sync::mpsc::Sender<waymark_timed::Opaque<T>>>;
+pub type Receiver<T, What = ()> = Rx<tokio::sync::mpsc::Receiver<waymark_timed::Opaque<T>>, What>;
+pub type UnboundedSender<T> = Tx<tokio::sync::mpsc::UnboundedSender<waymark_timed::Opaque<T>>>;
+pub type UnboundedReceiver<T, What = ()> =
+    Rx<tokio::sync::mpsc::UnboundedReceiver<waymark_timed::Opaque<T>>, What>;
+
+pub fn channel<T>(buffer: usize) -> (Sender<T>, Receiver<T, ()>) {
+    crate::wrap(tokio::sync::mpsc::channel(buffer))
+}
+
+pub fn unbounded_channel<T>() -> (UnboundedSender<T>, UnboundedReceiver<T, ()>) {
+    crate::wrap(tokio::sync::mpsc::unbounded_channel())
+}
+
+impl<T> Sender<T> {
+    pub async fn send(&self, value: T) -> Result<(), tokio::sync::mpsc::error::SendError<T>> {
+        self.channel
+            .send(waymark_timed::Opaque::wrap(value))
+            .await
+            .map_err(|val| {
+                let tokio::sync::mpsc::error::SendError(timed) = val;
+                tokio::sync::mpsc::error::SendError(timed.into_inner())
+            })
+    }
+}
+
+impl<T, What: crate::What> Receiver<T, What> {
+    pub async fn recv(&mut self) -> Option<T> {
+        self.channel
+            .recv()
+            .await
+            .map(|item| waymark_timed::Opaque::into_inner_measured(item, What::description()))
+    }
+}
+
+impl<T> UnboundedSender<T> {
+    pub fn send(&self, value: T) -> Result<(), tokio::sync::mpsc::error::SendError<T>> {
+        self.channel
+            .send(waymark_timed::Opaque::wrap(value))
+            .map_err(|val| {
+                let tokio::sync::mpsc::error::SendError(timed) = val;
+                tokio::sync::mpsc::error::SendError(timed.into_inner())
+            })
+    }
+}
+
+impl<T, What: crate::What> UnboundedReceiver<T, What> {
+    pub async fn recv(&mut self) -> Option<T> {
+        self.channel
+            .recv()
+            .await
+            .map(|item| waymark_timed::Opaque::into_inner_measured(item, What::description()))
+    }
+
+    pub fn try_recv(&mut self) -> Result<T, tokio::sync::mpsc::error::TryRecvError> {
+        self.channel
+            .try_recv()
+            .map(|item| waymark_timed::Opaque::into_inner_measured(item, What::description()))
+    }
+}
