76f2884c1cb7eff820b1c35a48bfb9aeb5e20129
MOZGIII <mozgiii@mzg.io>
2026-03-30 16:39:54 +0400

    Add the waymark_runloop_ticks_stats_inflight_actions_total metric

--- crates/lib/runloop/src/runloop.rs
+++ crates/lib/runloop/src/runloop.rs
@@ -799,6 +799,10 @@ where
             metrics::histogram!("waymark_runloop_ticks_stats_instances_done_pending_len")
                 .record(MetricsVal(instances_done_pending.len()));
 
+            let total_in_flight_actions: usize = inflight_actions.values().sum();
+            metrics::histogram!("waymark_runloop_ticks_stats_inflight_actions_total")
+                .record(MetricsVal(total_in_flight_actions));
+
             tracing::trace!(
                 target: "runloop-ticks",
 
