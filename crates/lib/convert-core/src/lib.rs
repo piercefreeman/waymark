c384ea309f4f252d2c7e97018ac390691600ed5f
MOZGIII <mozgiii@mzg.io>
2026-06-11 12:26:53 +0400

    Add convert-core crate

---
+++ crates/lib/convert-core/src/lib.rs
@@ -0,0 +1,42 @@
+//! Core conversion traits.
+
+#![warn(missing_docs)]
+
+/// Fallible conversion from one type to another.
+pub trait TryConvert<From, To> {
+    /// The error type returned when conversion fails.
+    type Error;
+
+    /// Convert `from` into `To`, or return an error.
+    fn try_convert(from: From) -> Result<To, Self::Error>;
+}
+
+/// Conversion from one type to another.
+pub trait Convert<From, To>: TryConvert<From, To, Error = core::convert::Infallible> {
+    /// Convert `from` into `To`.
+    fn convert(from: From) -> To;
+}
+
+impl<Converter, From, To> Convert<From, To> for Converter
+where
+    Converter: TryConvert<From, To, Error = core::convert::Infallible>,
+{
+    fn convert(from: From) -> To {
+        let Ok(to) = Converter::try_convert(from);
+        to
+    }
+}
+
+/// A converter based on [`core::convert::TryFrom`].
+pub struct StdConverter;
+
+impl<From, To> TryConvert<From, To> for StdConverter
+where
+    To: core::convert::TryFrom<From>,
+{
+    type Error = <To as core::convert::TryFrom<From>>::Error;
+
+    fn try_convert(from: From) -> Result<To, Self::Error> {
+        from.try_into()
+    }
+}
