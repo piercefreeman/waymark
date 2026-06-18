5d8bd9c1f57f54ea8fb63b24bebe445e8e1a2ae4
Pierce Freeman <piercefreeman@gmail.com>
2026-06-17 21:51:13 -0700

    Import website docs

---
+++ docs/navigation.ts
@@ -0,0 +1,54 @@
+export const navigation = [
+  {
+    title: 'Getting Started',
+    links: [
+      { title: 'Introduction', href: '/' },
+      { title: 'Quickstart', href: '/guides/quickstart' },
+      { title: 'Why Waymark', href: '/guides/motivation' },
+    ],
+  },
+  {
+    title: 'Concepts',
+    links: [
+      { title: 'Workflows & Actions', href: '/guides/workflows-and-actions' },
+      { title: 'Control Flow', href: '/guides/control-flow' },
+      { title: 'Retries & Timeouts', href: '/guides/retries' },
+      { title: 'Scheduled Workflows', href: '/guides/scheduling' },
+      { title: 'Configuration', href: '/guides/configuration' },
+      { title: 'Webapp', href: '/guides/webapp' },
+      { title: 'Production Deployment', href: '/guides/production' },
+    ],
+  },
+  {
+    title: 'Tutorials',
+    links: [
+      {
+        title: 'Building an Agent From Scratch',
+        href: '/tutorials/building-a-from-scratch-agent',
+      },
+      {
+        title: 'Durable Agents with Pydantic AI',
+        href: '/tutorials/durable-agents-with-pydantic-ai',
+      },
+      {
+        title: 'Integrating with FastAPI',
+        href: '/tutorials/integrating-with-fastapi',
+      },
+    ],
+  },
+  {
+    title: 'Python',
+    links: [
+      { title: 'Introduction to Python', href: '/python/authoring' },
+      { title: 'Workflow API', href: '/python/api/workflow' },
+      { title: 'Actions API', href: '/python/api/actions' },
+      { title: 'Retry Policy', href: '/python/api/retry' },
+      { title: 'Scheduling API', href: '/python/api/scheduling' },
+      { title: 'Exceptions', href: '/python/api/exceptions' },
+    ],
+  },
+  {
+    title: 'JavaScript',
+    links: [{ title: 'Next.js Spec', href: '/javascript/nextjs' }],
+  },
+]
