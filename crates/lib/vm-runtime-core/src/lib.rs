5d6e76c48d83cc7df0eb4b79f8c39e8c42d0cbcc
MOZGIII <mozgiii@mzg.io>
2026-07-22 11:53:01 +0400

    Implement select continuation machinery at vm-runtime-core

--- crates/lib/vm-runtime-core/src/lib.rs
+++ crates/lib/vm-runtime-core/src/lib.rs
@@ -8,8 +8,10 @@ mod frame;
 mod frame_exception_handlers;
 mod promise_state;
 mod promise_states;
+mod promise_waiter;
 mod registers;
 mod runtime_state;
+mod select_states;
 
 pub use self::capture_runtime_view::*;
 pub use self::continuation::*;
@@ -17,5 +19,7 @@ pub use self::frame::*;
 pub use self::frame_exception_handlers::*;
 pub use self::promise_state::*;
 pub use self::promise_states::*;
+pub use self::promise_waiter::*;
 pub use self::registers::*;
 pub use self::runtime_state::*;
+pub use self::select_states::*;
