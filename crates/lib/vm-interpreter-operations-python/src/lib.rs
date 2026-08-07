447b2711f7742218d791b6992574e1107479cc29
MOZGIII <mozgiii@mzg.io>
2026-08-08 02:16:28 +0400

    Add vm-interpreter-operations-python crate

---
+++ crates/lib/vm-interpreter-operations-python/src/lib.rs
@@ -0,0 +1,6 @@
+//! The Python variation of the VM interpreter operations.
+
+#![warn(missing_docs)]
+
+/// The Python variation marker for the VM interpreter operations.
+pub enum PythonVariation {}
