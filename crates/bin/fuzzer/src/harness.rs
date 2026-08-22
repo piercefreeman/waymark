b6590683d4f7316dd8d6f1d9b425027526c5e5f8
MOZGIII <mozgiii@mzg.io>
2026-08-22 17:44:29 +0400

    Split the python value converters into separate purpose-oritented types

--- crates/bin/fuzzer/src/harness.rs
+++ crates/bin/fuzzer/src/harness.rs
@@ -25,7 +25,13 @@ where
         > + Send
         + 'static,
 {
-    inline_action::<waymark_vm_value_python_convert_proto::Converter, _, _, _>(body)
+    inline_action::<
+        waymark_vm_value_python_convert_proto::ActionArgumentsConverter,
+        waymark_vm_value_python_convert_proto::ActionOutcomeConverter,
+        _,
+        _,
+        _,
+    >(body)
 }
 
 pub async fn run_case(
