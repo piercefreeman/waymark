5dc02a6cde36c0908a67ecf971f9c3610d7ce703
MOZGIII <mozgiii@mzg.io>
2026-09-15 21:15:59 +0400

    Bring the tokio metrics reporters up through the managed spawner

--- crates/lib/tokio-metrics-bringup/src/lib.rs
+++ crates/lib/tokio-metrics-bringup/src/lib.rs
@@ -9,23 +9,42 @@ fn make_metric_name_transformer(
     }
 }
 
-// TODO: update spawns when we have a proper
-// task management / registration system.
-pub fn bringup(executable_name: &'static str) -> tokio_metrics::TaskMonitor {
+pub fn start<Spawner>(
+    mut spawner: Spawner,
+    executable_name: &'static str,
+    shutdown_token: tokio_util::sync::CancellationToken,
+) -> tokio_metrics::TaskMonitor
+where
+    Spawner: waymark_managed_spawner::Spawner,
+{
     let metric_name_transformer = make_metric_name_transformer(executable_name);
 
-    tokio::spawn(
-        tokio_metrics::RuntimeMetricsReporterBuilder::default()
-            .with_metrics_transformer(metric_name_transformer)
-            .describe_and_run(),
-    );
+    spawner.spawn("tokio runtime metrics reporter", {
+        let shutdown_token = shutdown_token.clone();
+        async move {
+            shutdown_token
+                .run_until_cancelled(
+                    tokio_metrics::RuntimeMetricsReporterBuilder::default()
+                        .with_metrics_transformer(metric_name_transformer)
+                        .describe_and_run(),
+                )
+                .await;
+        }
+    });
 
     let task_monitor = tokio_metrics::TaskMonitor::new();
 
-    tokio::spawn(
-        tokio_metrics::TaskMetricsReporterBuilder::new(metric_name_transformer)
-            .describe_and_run(task_monitor.clone()),
-    );
+    spawner.spawn("tokio task metrics reporter", {
+        let task_monitor = task_monitor.clone();
+        async move {
+            shutdown_token
+                .run_until_cancelled(
+                    tokio_metrics::TaskMetricsReporterBuilder::new(metric_name_transformer)
+                        .describe_and_run(task_monitor),
+                )
+                .await;
+        }
+    });
 
     task_monitor
 }
